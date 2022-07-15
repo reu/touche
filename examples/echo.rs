f8ad237bb7f57fd6aec664f73ab7d7b7e370eb85
Rodrigo Navarro <rnavarro1@gmail.com>
2022-07-15 15:51:22 -0300

    Generalize the request handler to a trait.

--- examples/echo.rs
+++ examples/echo.rs
@@ -1,15 +1,18 @@
 use std::net::TcpListener;
 
 use http::{Response, StatusCode};
+use shrike::Request;
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:4444")?;
 
-    let (mut stream, _addr) = listener.accept()?;
+    for stream in listener.incoming() {
+        shrike::serve(&mut stream?, |req: Request| {
+            Response::builder()
+                .status(StatusCode::OK)
+                .body(req.into_body().as_bytes().into())
+        })?;
+    }
 
-    shrike::serve(&mut stream, |req| {
-        Response::builder()
-            .status(StatusCode::OK)
-            .body(req.into_body().as_bytes().into())
-    })
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,41 @@ use std::{
     net::TcpStream,
 };
 
-use http::{Request, Response};
+pub use request::Body as RequestBody;
+pub use response::Body as ResponseBody;
+
+pub type Request = http::Request<RequestBody>;
+pub type Response = http::Response<ResponseBody>;
+
+pub trait Handler<Err>: Sync + Send
+where
+    Err: Into<Box<dyn Error + Send + Sync>>,
+{
+    fn handle(&self, request: Request) -> Result<Response, Err>;
+}
+
+impl<F, Err> Handler<Err> for F
+where
+    F: Fn(Request) -> Result<Response, Err>,
+    F: Sync + Send,
+    Err: Into<Box<dyn Error + Send + Sync>>,
+{
+    fn handle(&self, request: Request) -> Result<Response, Err> {
+        self(request)
+    }
+}
 
 pub fn serve<Handle, Err>(stream: &mut TcpStream, handle: Handle) -> io::Result<()>
 where
-    Handle: Fn(Request<request::Body>) -> Result<Response<response::Body>, Err>,
+    Handle: Handler<Err>,
     Err: Into<Box<dyn Error + Send + Sync>>,
 {
     let req_stream = BufReader::new(stream.try_clone()?);
     match request::parse_request(req_stream) {
         Ok(req) => {
-            let res = handle(req).map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+            let res = handle
+                .handle(req)
+                .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
             let mut res_stream = BufWriter::new(stream);
             response::write_response(res, &mut res_stream)?;
             res_stream.flush()?;
