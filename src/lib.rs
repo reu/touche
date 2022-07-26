83bdacb3d8842d69ccd082323e876322a35d403b
Rodrigo Navarro <rnavarro1@gmail.com>
2022-07-26 14:23:49 -0300

    Implementation of the http client.

--- src/lib.rs
+++ src/lib.rs
@@ -134,3 +134,155 @@ where
 
     Ok(())
 }
+
+pub enum ConnectionOutcome {
+    Close,
+    KeepAlive(Connection),
+}
+
+impl ConnectionOutcome {
+    pub fn unwrap(self) -> Connection {
+        match self {
+            ConnectionOutcome::KeepAlive(conn) => conn,
+            ConnectionOutcome::Close => panic!("Connection closed"),
+        }
+    }
+}
+
+pub fn send<Conn, B>(
+    connection: Conn,
+    req: http::Request<B>,
+) -> io::Result<(ConnectionOutcome, http::Response<Body>)>
+where
+    Conn: Into<Connection>,
+    B: HttpBody,
+{
+    let conn = connection.into();
+
+    let reader = BufReader::new(conn.clone());
+    let mut writer = BufWriter::new(conn);
+
+    request::write_request(req, &mut writer)?;
+    writer.flush()?;
+
+    let res = response::parse_response(reader)
+        .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+
+    let asks_for_close = res
+        .headers()
+        .typed_get::<headers::Connection>()
+        .filter(|conn| conn.contains("close"))
+        .is_some();
+
+    let outcome = if asks_for_close {
+        ConnectionOutcome::Close
+    } else {
+        ConnectionOutcome::KeepAlive(writer.into_inner()?)
+    };
+
+    Ok((outcome, res))
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        io::Cursor,
+        net::{TcpListener, TcpStream},
+        thread,
+    };
+
+    use super::*;
+
+    #[test]
+    fn send_request() {
+        let listener = TcpListener::bind("0.0.0.0:0").unwrap();
+        let port = listener.local_addr().unwrap().port();
+
+        thread::spawn(move || {
+            let (conn, _) = listener.accept().unwrap();
+            serve(conn, |req: Request| {
+                http::Response::builder().body(req.into_body())
+            })
+            .ok();
+        });
+
+        let conn = TcpStream::connect(("0.0.0.0", port)).unwrap();
+
+        let req = http::Request::builder().body("Hello world").unwrap();
+        let (conn, res) = send(conn, req).unwrap();
+        assert_eq!(res.into_body().into_bytes().unwrap(), b"Hello world");
+
+        let req = http::Request::builder().body("Bye world").unwrap();
+        let (conn, res) = send(conn.unwrap(), req).unwrap();
+        assert_eq!(res.into_body().into_bytes().unwrap(), b"Bye world");
+
+        let req = http::Request::builder().body(()).unwrap();
+        let (conn, res) = send(conn.unwrap(), req).unwrap();
+        assert_eq!(res.into_body().into_bytes().unwrap(), b"");
+
+        let req = http::Request::builder()
+            .header("transfer-encoding", "chunked")
+            .body(Body::from_iter(vec![&b"lol"[..], &b"wut"[..]]))
+            .unwrap();
+        let (_conn, res) = send(conn.unwrap(), req).unwrap();
+        assert_eq!(res.into_body().into_bytes().unwrap(), b"lolwut");
+    }
+
+    #[test]
+    fn correctly_handles_closing_connections() {
+        let listener = TcpListener::bind("0.0.0.0:0").unwrap();
+        let port = listener.local_addr().unwrap().port();
+
+        thread::spawn(move || {
+            let (conn, _) = listener.accept().unwrap();
+            serve(conn, |_req| {
+                http::Response::builder()
+                    .header("connection", "close")
+                    .body(Body::from_reader(Cursor::new(b"lolwut"), None))
+            })
+            .ok();
+        });
+
+        let conn = TcpStream::connect(("0.0.0.0", port)).unwrap();
+
+        let req = http::Request::builder().body(()).unwrap();
+        let (conn, res) = send(conn, req).unwrap();
+
+        assert_eq!(res.into_body().into_bytes().unwrap(), b"lolwut");
+        assert!(matches!(conn, ConnectionOutcome::Close));
+    }
+
+    #[test]
+    fn keep_http_10_connection_alive_when_asked_to() {
+        let listener = TcpListener::bind("0.0.0.0:0").unwrap();
+        let port = listener.local_addr().unwrap().port();
+
+        thread::spawn(move || {
+            let (conn, _) = listener.accept().unwrap();
+            serve(conn, |_req| http::Response::builder().body("lolwut")).unwrap();
+        });
+
+        let conn = TcpStream::connect(("0.0.0.0", port)).unwrap();
+
+        let req = http::Request::builder()
+            .version(Version::HTTP_10)
+            .header("connection", "keep-alive")
+            .body(())
+            .unwrap();
+
+        let (conn, res) = send(conn, req).unwrap();
+
+        assert_eq!(res.into_body().into_bytes().unwrap(), b"lolwut");
+        assert!(matches!(conn, ConnectionOutcome::KeepAlive(_)));
+
+        let req = http::Request::builder()
+            .version(Version::HTTP_10)
+            .body(())
+            .unwrap();
+
+        let (conn, res) = send(conn.unwrap(), req).unwrap();
+
+        assert_eq!(res.into_body().into_bytes().unwrap(), b"lolwut");
+        assert!(matches!(conn, ConnectionOutcome::Close));
+    }
+}
