7b13da66ce8b69586340acd65910a613cf458b02
Rodrigo Navarro <rnavarro1@gmail.com>
2022-07-29 08:48:43 -0300

    Add Server documentation.

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,25 @@
+//! HTTP Server
+//!
+//! The [`Server`] is responsible to read and parse a [`http::Request`], and then execute a [`App`] to generate
+//! a [`http::Response`].
+//!
+//! The implementation follows a simple thead per connection model, backed by a thread pool.
+//!
+//! # Example
+//! ```no_run
+//! use touche::{Response, Server, StatusCode};
+//!
+//! fn main() -> std::io::Result<()> {
+//!     Server::builder()
+//!         .max_threads(256)
+//!         .bind("0.0.0.0:4444")
+//!         .serve(|_req| {
+//!             Response::builder()
+//!                 .status(StatusCode::OK)
+//!                 .body(())
+//!         })
+//! }
+//! ```
 use std::{
     error::Error,
     io,
@@ -8,12 +30,41 @@ use threadpool::ThreadPool;
 
 use crate::{connection::Connection, serve, App};
 
+/// A listening HTTP server that accepts HTTP 1 connections.
 pub struct Server<'a> {
     thread_pool: ThreadPool,
     incoming: Box<dyn Iterator<Item = Connection> + 'a>,
 }
 
 impl<'a> Server<'a> {
+    /// Starts the [`ServerBuilder`].
+    pub fn builder() -> ServerBuilder {
+        Default::default()
+    }
+
+    /// Binds the [`Server`] to the given `addr`.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if binding to the address fails. For a non panic method to bind the
+    /// server, see [`ServerBuilder::try_bind`].
+    pub fn bind<A: ToSocketAddrs>(addr: A) -> Server<'static> {
+        Self::builder().bind(addr)
+    }
+
+    /// Serves an [`App`].
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use touche::{Request, Response, Server, StatusCode};
+    /// # fn main() -> std::io::Result<()> {
+    /// Server::bind("0.0.0.0:4444").serve(|req: Request<_>| {
+    ///     Response::builder()
+    ///         .status(StatusCode::OK)
+    ///         .body(req.into_body())
+    /// })
+    /// # }
+    /// ```
     pub fn serve<A>(self, app: A) -> io::Result<()>
     where
         A: App,
@@ -29,6 +80,27 @@ impl<'a> Server<'a> {
         Ok(())
     }
 
+    /// Serves an [`Connection`]. This should be used when you need to execute some logic on every
+    /// connection.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use std::convert::Infallible;
+    /// # use touche::{Connection, Response, Server, StatusCode};
+    /// # fn main() -> std::io::Result<()> {
+    /// Server::builder()
+    ///     .bind("0.0.0.0:4444")
+    ///     .serve_connection(|conn: &Connection| {
+    ///         println!("New connection arrived: {:?}", conn.peer_addr());
+    ///
+    ///         Ok::<_, Infallible>(|_req| {
+    ///             Response::builder()
+    ///                 .status(StatusCode::OK)
+    ///                 .body(())
+    ///         })
+    ///     })
+    /// # }
+    /// ```
     pub fn serve_connection<C>(self, app: C) -> io::Result<()>
     where
         C: ConnectionHandler,
@@ -45,14 +117,6 @@ impl<'a> Server<'a> {
 
         Ok(())
     }
-
-    pub fn builder() -> ServerBuilder {
-        Default::default()
-    }
-
-    pub fn bind<A: ToSocketAddrs>(addr: A) -> Server<'static> {
-        Self::builder().bind(addr)
-    }
 }
 
 pub struct ServerBuilder {
@@ -66,10 +130,67 @@ impl Default for ServerBuilder {
 }
 
 impl ServerBuilder {
+    /// Define the max number of threads this server may create. Defaults to `512`.
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use touche::{Response, Server, StatusCode};
+    /// # fn main() -> std::io::Result<()> {
+    /// Server::builder()
+    ///     .max_threads(12)
+    ///     .bind("0.0.0.0:4444")
+    ///     .serve(|_req| {
+    ///         Response::builder()
+    ///             .status(StatusCode::OK)
+    ///             .body(())
+    ///     })
+    /// # }
+    /// ```
     pub fn max_threads(self, max_threads: usize) -> Self {
         Self { max_threads }
     }
 
+    /// Binds the [`Server`] to the given `addr`.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if binding to the address fails. For a non panic way to bind a
+    /// server, see [`ServerBuilder::try_bind`].
+    pub fn bind<A: ToSocketAddrs>(self, addr: A) -> Server<'static> {
+        self.try_bind(addr).unwrap()
+    }
+
+    /// Tries to bind the server to the informed `addr`.
+    pub fn try_bind<A: ToSocketAddrs>(self, addr: A) -> io::Result<Server<'static>> {
+        let listener = TcpListener::bind(addr)?;
+        Ok(self.from_connections(Box::new(TcpAcceptor { listener })))
+    }
+
+    /// Accepts connections from some [`Iterator`].
+    ///
+    /// # Example
+    /// Running the server on a Unix socket
+    /// ```no_run
+    /// # use std::os::unix::net::UnixListener;
+    /// # use touche::{Request, Response, Server, StatusCode};
+    /// # fn main() -> std::io::Result<()> {
+    /// let listener = UnixListener::bind("touche.socket")?;
+    ///
+    /// // Converting the Unix socket to a compatible `Connection`
+    /// let connections = listener
+    ///     .incoming()
+    ///     .filter_map(|conn| conn.ok())
+    ///     .map(|conn| conn.into());
+    ///
+    /// Server::builder()
+    ///     .from_connections(connections)
+    ///     .serve(|_req| {
+    ///         Response::builder()
+    ///             .status(StatusCode::OK)
+    ///             .body("Hello from Unix socket!")
+    ///     })
+    /// # }
+    /// ```
     pub fn from_connections<'a, T: IntoIterator<Item = Connection> + 'a>(
         self,
         conns: T,
@@ -78,15 +199,6 @@ impl ServerBuilder {
             thread_pool: ThreadPool::new(self.max_threads),
             incoming: Box::new(conns.into_iter()),
         }
-    }
-
-    pub fn bind<A: ToSocketAddrs>(self, addr: A) -> Server<'static> {
-        self.try_bind(addr).unwrap()
-    }
-
-    pub fn try_bind<A: ToSocketAddrs>(self, addr: A) -> io::Result<Server<'static>> {
-        let listener = TcpListener::bind(addr)?;
-        Ok(self.from_connections(Box::new(TcpAcceptor { listener })))
     }
 }
 
