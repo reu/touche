b8e315252dfc58cbc2fdda07d9ab378d76693f0a
Rodrigo Navarro <rnavarro1@gmail.com>
2022-07-30 18:40:43 -0300

    Add websockets with Tungstenite example.

---
+++ examples/websocket.rs
@@ -0,0 +1,22 @@
+use std::error::Error;
+
+use touche::{upgrade::Upgrade, Body, Connection, Request, Server};
+use tungstenite::{protocol::Role, WebSocket};
+
+fn main() -> std::io::Result<()> {
+    Server::bind("0.0.0.0:4444").serve(|req: Request<Body>| {
+        let res = tungstenite::handshake::server::create_response(&req.map(|_| ()))?;
+
+        Ok::<_, Box<dyn Error + Send + Sync>>(res.upgrade(|stream: Connection| {
+            let mut ws = WebSocket::from_raw_socket(stream, Role::Server, None);
+
+            while let Ok(msg) = ws.read_message() {
+                if msg.is_text() {
+                    if let Err(_) = ws.write_message(msg) {
+                        break;
+                    }
+                }
+            }
+        }))
+    })
+}
