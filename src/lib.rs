4f60fa417bcf4b606986a735d06ff9c4290caee0
Rodrigo Navarro <rnavarro1@gmail.com>
2023-03-13 18:53:53 -0300

    Client feature flag.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 #![doc = include_str!("../README.md")]
 
 pub mod body;
+#[cfg(feature = "client")]
 pub mod client;
 mod connection;
 mod read_queue;
 mod request;
 mod response;
+#[cfg(feature = "server")]
 pub mod server;
 #[cfg(feature = "rustls")]
 mod tls;
@@ -13,6 +15,9 @@ pub mod upgrade;
 
 pub use body::Body;
 pub use body::HttpBody;
+#[cfg(feature = "client")]
+pub use client::Client;
 pub use connection::Connection;
 pub use http::{header, Method, Request, Response, StatusCode, Uri, Version};
+#[cfg(feature = "server")]
 pub use server::Server;
