ea9e0ab891f3c99b994a650f414c0995728be311
Rodrigo Navarro <rnavarro1@gmail.com>
2025-01-28 22:26:51 -0300

    Make Service::call() take &mut self (#9)
    Credits to @petoknm
    
    ---------
    
    Co-authored-by: Peter Majchrak <petoknm@gmail.com>

--- examples/expect-continue.rs
+++ examples/expect-continue.rs
@@ -12,14 +12,14 @@ impl Service for UploadService {
     type Body = &'static str;
     type Error = Infallible;
 
-    fn call(&self, _req: Request<Body>) -> Result<http::Response<Self::Body>, Self::Error> {
+    fn call(&mut self, _req: Request<Body>) -> Result<http::Response<Self::Body>, Self::Error> {
         Ok(Response::builder()
             .status(StatusCode::OK)
             .body("Thanks for the info!")
             .unwrap())
     }
 
-    fn should_continue(&self, req: &Request<Body>) -> StatusCode {
+    fn should_continue(&mut self, req: &Request<Body>) -> StatusCode {
         match req.headers().typed_get::<headers::ContentLength>() {
             Some(len) if len.0 <= self.max_length => StatusCode::CONTINUE,
             _ => StatusCode::EXPECTATION_FAILED,
