4acca76e2440f370d324b19756141b7815b08d00
Rodrigo Navarro <rnavarro1@gmail.com>
2025-02-07 22:01:32 -0300

    Simplify TLS example

--- examples/rustls.rs
+++ examples/rustls.rs
@@ -1,17 +1,10 @@
-#[cfg(not(feature = "rustls"))]
-fn main() {
-    println!("This example requires the rustls feature to be enabled");
-}
-
+// Run with: curl --insecure https://localhost:4444
 #[cfg(feature = "rustls")]
-fn main() -> std::io::Result<()> {
-    use std::{
-        io::{self, ErrorKind::Other},
-        net::TcpListener,
-        sync::Arc,
-    };
+fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    use std::{net::TcpListener, sync::Arc};
 
     use rustls::{crypto, ServerConfig, ServerConnection, StreamOwned};
+    use rustls_pki_types::{pem::PemObject, CertificateDer, PrivateKeyDer};
     use touche::{Response, Server, StatusCode};
 
     crypto::ring::default_provider().install_default().ok();
@@ -19,13 +12,15 @@ fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("0.0.0.0:4444")?;
 
     let tls_cfg = {
-        let certs = certs::load_certs("examples/tls/cert.pem")?;
-        let key = certs::load_private_key("examples/tls/key.pem")?;
+        let certs = CertificateDer::pem_file_iter("examples/tls/cert.pem")?
+            .filter_map(|cert| cert.ok())
+            .collect();
+
+        let key = PrivateKeyDer::from_pem_file("examples/tls/key.pem")?;
 
         let cfg = ServerConfig::builder()
             .with_no_client_auth()
-            .with_single_cert(certs, key)
-            .map_err(|e| io::Error::new(Other, e))?;
+            .with_single_cert(certs, key)?;
 
         Arc::new(cfg)
     };
@@ -47,23 +42,12 @@ fn main() -> std::io::Result<()> {
             Response::builder()
                 .status(StatusCode::OK)
                 .body("Hello from TLS")
-        })
+        })?;
+
+    Ok(())
 }
 
-#[cfg(feature = "rustls")]
-mod certs {
-    use std::io::{self, ErrorKind::Other};
-
-    use rustls_pki_types::{pem::PemObject, CertificateDer, PrivateKeyDer};
-
-    pub fn load_certs(filename: &str) -> io::Result<Vec<CertificateDer<'static>>> {
-        CertificateDer::pem_file_iter(filename)
-            .map_err(|err| io::Error::new(Other, err))?
-            .map(|cert| cert.map_err(|err| io::Error::new(Other, err)))
-            .collect()
-    }
-
-    pub fn load_private_key(filename: &str) -> io::Result<PrivateKeyDer<'static>> {
-        PrivateKeyDer::from_pem_file(filename).map_err(|err| io::Error::new(Other, err))
-    }
+#[cfg(not(feature = "rustls"))]
+fn main() {
+    println!("This example requires the rustls feature to be enabled");
 }
