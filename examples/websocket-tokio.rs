6103ae64a0194405363ccaea72b1a81a17c65622
Rodrigo Navarro <rnavarro1@gmail.com>
2025-02-07 23:51:04 -0300

    Typo

--- examples/websocket-tokio.rs
+++ examples/websocket-tokio.rs
@@ -11,8 +11,8 @@ fn main() -> std::io::Result<()> {
 
     Server::builder()
         .bind("0.0.0.0:4444")
-        // We can have can handle multiple websockets even with a single thread server, since the
-        // websocket connections will be handled by Tokio and not by Touche.
+        // We can handle multiple websockets even with a single thread server, since the websocket
+        // connections will be handled by Tokio and not by Touche.
         .serve_single_thread(move |req: Request<Body>| {
             let tokio_handle = tokio_handle.clone();
 
